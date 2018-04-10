90c56545d13e3d3fcdc2abf9a523c7815ecca59f
fwtravisbain <fwtravisbain@outlook.com>
2018-04-10 13:39:05 -0700

    Initial Commit

---
+++ GithubLab/app/src/main/java/edu/tacoma/uw/css/fwtbain/githublab/GithubActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.fwtbain.githublab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GithubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github);
+    }
+}
